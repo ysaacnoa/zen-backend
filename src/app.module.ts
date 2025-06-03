a1b4f1a64e5005a5ace699ba2249911994c7872d
ysaacnoa <ysaacnoe.correa@unmsm.edu.pe>
2025-06-02 23:00:32 -0500

    ♻️ (app): add gamificactiono module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { GamificationModule } from './gamification/gamification.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, GamificationModule],
   controllers: [AppController],
   providers: [AppService],
 })
