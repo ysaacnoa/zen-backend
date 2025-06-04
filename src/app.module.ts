b4cd943671c67564e10d0917cfae793f4f315917
ysaacnoa <ysaacnoe.correa@unmsm.edu.pe>
2025-06-03 22:17:55 -0500

    ♻️ (app): include ia module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { IaModule } from './ia/ia.module';
 import { AuthModule } from './auth/auth.module';
 import { GamificationModule } from './gamification/gamification.module';
 import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [AuthModule, GamificationModule, UserModule],
+  imports: [IaModule, AuthModule, GamificationModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
