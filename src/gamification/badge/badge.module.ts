2f5270bffd3ab89d09418d28a30b9361c113073c
ysaacnoa <ysaacnoe.correa@unmsm.edu.pe>
2025-06-07 14:37:34 -0500

    ♻️ (badges): update badge logic

--- src/gamification/badge/badge.module.ts
+++ src/gamification/badge/badge.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { BadgeService } from './badge.service';
 import { BadgeController } from './badge.controller';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   providers: [BadgeService],
   controllers: [BadgeController],
   exports: [BadgeService],
