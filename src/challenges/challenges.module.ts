e23150cd1532be2130d4ce64b8a605a09045d289
ysaacnoa <ysaacnoe.correa@unmsm.edu.pe>
2025-06-10 20:18:58 -0500

    ➖ (challenge): remove scheduler

--- src/challenges/challenges.module.ts
+++ src/challenges/challenges.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { ScheduleModule } from '@nestjs/schedule';
 import { ChallengesController } from './challenges.controller';
 import { ChallengesService } from './challenges.service';
-import { ChallengesScheduler } from './challenges.scheduler';
 import { IaModule } from '../ia/ia.module';
 import { PrismaModule } from '../prisma/prisma.module';
 import { UserModule } from 'src/user/user.module';
@@ -10,7 +9,7 @@ import { UserModule } from 'src/user/user.module';
 @Module({
   imports: [ScheduleModule.forRoot(), IaModule, PrismaModule, UserModule],
   controllers: [ChallengesController],
-  providers: [ChallengesService, ChallengesScheduler],
+  providers: [ChallengesService],
   exports: [ChallengesService],
 })
 export class ChallengesModule {}
